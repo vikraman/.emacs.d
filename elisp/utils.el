48859920f4cc11199acf843398d6c56c65da43b0
Vikraman Choudhury <git@vikraman.org>
2015-02-18 01:50:39 +0530

    Port more stuff to `use-package'

--- elisp/utils.el
+++ elisp/utils.el
@@ -3,145 +3,99 @@
 
 ;;; Code:
 
+(require 'use-package)
+
 ;; ace-jump-mode
-(require 'ace-jump-mode)
+(use-package ace-jump-mode
+  :ensure t
+  :config (ace-jump-mode-enable-mark-sync)
+  :bind (("C-." . ace-jump-mode)
+         ("C-," . ace-jump-mode-pop-mark)))
 
-;; auto-complete
-;; (require 'auto-complete)
-;; (ac-config-default)
-;; ;; Store the completion history in the cache directory
-;; (setq ac-comphist-file "~/.emacs.cache/ac-comphist.dat")
-;; (setq-default ac-sources '(ac-source-semantic
-;;                            ac-source-yasnippet
-;;                            ac-source-abbrev
-;;                            ac-source-words-in-buffer
-;;                            ac-source-words-in-same-mode-buffers
-;;                            ac-source-files-in-current-dir
-;;                            ))
-
-;; (setq ac-delay 0.0)
-;; (setq ac-auto-show-menu 0.0)
-;; (setq ac-show-menu-immediately-on-auto-complete t)
-;; (setq ac-quick-help-delay 1.0)
-;; (setq ac-menu-height 3)
-;; (setq ac-auto-start 2)
-;; (setq ac-use-menu-map t)
-
-;; (add-to-list 'ac-modes 'web-mode)
-;; (add-to-list 'ac-modes 'scss-mode)
-
-;; (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-;; Enable auto-complete globally
-;; (global-auto-complete-mode t)
-
-;; (eval-after-load 'flyspell
-;;   '(eval-after-load 'auto-complete
-;;      '(ac-flyspell-workaround)))
-
-;; ac-ispell
-;; (custom-set-variables
-;;  '(ac-ispell-requires 4)
-;;  '(ac-ispell-fuzzy-limit 4))
-
-;; (eval-after-load 'auto-complete
-;;   '(lambda ()
-;;      (ac-ispell-setup)
-;;      (ac-ispell-ac-setup)))
-
-;; auto-fill-mode
+;; auto-fill
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 ;; company-mode
-(setq company-idle-delay 0.2)
-(setq company-minimum-prefix-length 2)
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.2)
+  (setq company-minimum-prefix-length 2)
+  (add-hook 'after-init-hook 'global-company-mode))
 
 ;; TeX
 (load "tex-init")
 
 ;; ag -- the silver searcher
-(require 'ag)
-(setq ag-highlight-search t)
-(setq ag-reuse-window 't)
-(setq ag-reuse-buffers 't)
-
-;; cedet
-;(require 'cedet)
-;(when (featurep 'cedet)
-;  (global-ede-mode 1)
-;  (setq semantic-load-turn-everything-on t)
-;  (semantic-load-enable-gaudy-code-helpers)
-;  (semantic-load-enable-excessive-code-helpers)
-;  (semantic-load-enable-all-exuberent-ctags-support)
-;  (global-semanticdb-minor-mode 1)
-;  (global-srecode-minor-mode 1)
-;  (setq semanticdb-default-save-directory "~/.emacs.cache/semanticdb")
-;  (setq semanticdb-default-system-save-directory "~/.emacs.cache/semanticdb"))
-
-;; autopair
-;; (require 'autopair)
-;; (when (featurep 'autopair)
-;;   (autopair-global-mode))
+(use-package ag
+  :ensure t
+  :config
+  (setq ag-highlight-search t)
+  (setq ag-reuse-window 't)
+  (setq ag-reuse-buffers 't))
 
 ;; css-mode
-(add-hook 'css-mode-hook
-          (setq css-indent-offset 2))
+(use-package css-mode
+  :ensure t
+  :config
+  (add-hook 'css-mode-hook (setq css-indent-offset 2)))
 
 ;; dired
-(require 'dired+)
-(require 'dired-details)
-(dired-details-install)
-(require 'dired-details+)
+(use-package dired+ :ensure t)
+(use-package dired-details
+  :ensure t
+  :init (dired-details-install))
+(use-package dired-details+ :ensure t)
 
 ;; edit-server
-(when (and
-       (require 'edit-server nil t)
-       (daemonp))
-  (edit-server-start))
-
-;; erc
-(setq  erc-server-coding-system '(utf-8 . utf-8))
+(use-package edit-server
+  :ensure t
+  :if (and (window-system) (daemonp))
+  :init (edit-server-start))
 
 ;; expand-region
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
 
 ;; agda2-mode
-(require 'agda2)
-(add-hook 'agda2-mode-hook
-          '(lambda ()
-             (setq agda2-include-dirs '("." "/usr/share/agda-stdlib"))
-             ))
-(setq agda2-highlight-set-faces nil)
-(defface agda2-highlight-primitive-type-face
-  '((t (:foreground "dark cyan")))
-  "The face used for primitive types (like Set and Prop)."
-)
-(defface agda2-highlight-datatype-face
-  '((t (:foreground "dark cyan")))
-  "The face used for datatypes."
-)
-(defface agda2-highlight-function-face
-  '((t (:foreground "dark cyan")))
-  "The face used for functions."
-)
-(defface agda2-highlight-postulate-face
-  '((t (:foreground "dark cyan")))
-  "The face used for postulates."
-)
-(defface agda2-highlight-primitive-face
-  '((t (:foreground "dark cyan")))
-  "The face used for primitive functions."
-)
-(defface agda2-highlight-record-face
-  '((t (:foreground "dark cyan")))
-  "The face used for record types."
-)
+(use-package agda2
+  :config
+  (add-hook 'agda2-mode-hook
+            '(lambda ()
+               (setq agda2-include-dirs '("." "/usr/share/agda-stdlib"))))
+  (setq agda2-highlight-set-faces nil)
+  (defface agda2-highlight-primitive-type-face
+    '((t (:foreground "dark cyan")))
+    "The face used for primitive types (like Set and Prop)."
+    )
+  (defface agda2-highlight-datatype-face
+    '((t (:foreground "dark cyan")))
+    "The face used for datatypes."
+    )
+  (defface agda2-highlight-function-face
+    '((t (:foreground "dark cyan")))
+    "The face used for functions."
+    )
+  (defface agda2-highlight-postulate-face
+    '((t (:foreground "dark cyan")))
+    "The face used for postulates."
+    )
+  (defface agda2-highlight-primitive-face
+    '((t (:foreground "dark cyan")))
+    "The face used for primitive functions."
+    )
+  (defface agda2-highlight-record-face
+    '((t (:foreground "dark cyan")))
+    "The face used for record types."
+    ))
 
 ;; vc-darcs
-(add-to-list 'vc-handled-backends 'DARCS)
-(autoload 'vc-darcs-find-file-hook "vc-darcs")
-(add-hook 'find-file-hooks 'vc-darcs-find-file-hook)
+(use-package vc-darcs
+  :ensure t
+  :config
+  (add-to-list 'vc-handled-backends 'DARCS)
+  (add-hook 'find-file-hooks 'vc-darcs-find-file-hook))
 
 ;; dart
 (load "dart-init")
@@ -151,7 +105,11 @@
 (add-to-list 'auto-mode-alist '("\\.xtm$" . extempore-mode))
 
 ;; font-lock
-(global-font-lock-mode 1)
+(use-package font-lock
+  :init (global-font-lock-mode 1))
+
+;; geiser
+(use-package geiser :ensure t)
 
 ;; haskell
 (load "haskell-init")
@@ -161,137 +119,156 @@
 ;; (autoload 'highlight-indentation-current-column-mode "highlight-indentation" nil t)
 
 ;; ido
-(ido-mode 1)
-(ido-everywhere 1)
-(setq ido-enable-flex-matching t)
-(setq ido-save-directory-list-file "~/.emacs.cache/ido.last")
-(setq ido-use-filename-at-point 'guess)
-(require 'ido-ubiquitous)
-(ido-ubiquitous-mode 1)
+(use-package ido
+  :init (progn
+          (ido-mode 1)
+          (ido-everywhere 1))
+  :config
+  (setq ido-enable-flex-matching t)
+  (setq ido-save-directory-list-file "~/.emacs.cache/ido.last")
+  (setq ido-use-filename-at-point 'guess))
+
+(use-package ido-ubiquitous
+  :ensure t
+  :init (ido-ubiquitous-mode 1))
 
 ;; ispell
-(when (executable-find "aspell")
+(use-package ispell
+  :if (executable-find "aspell")
+  :config
   (setq ispell-program-name "aspell"
         ispell-extra-args '("--sug-mode=ultra")))
-(defun c/enable-flyspell ()
-  (flyspell-mode 1))
-(defun c/enable-flyspell-prog ()
-  (flyspell-prog-mode))
-(add-hook 'text-mode-hook 'c/enable-flyspell)
-(add-hook 'message-mode-hook 'c/enable-flyspell)
-(add-hook 'magit-log-edit-mode-hook 'c/enable-flyspell)
+
+;; flyspell
+(use-package flyspell
+  :config
+  (add-hook 'text-mode-hook 'flyspell-mode)
+  (add-hook 'message-mode-hook 'flyspell-mode)
+  (add-hook 'magit-log-edit-mode-hook 'flyspell-prog-mode))
 
 ;; langtool
-(require 'langtool)
-(setq langtool-language-tool-jar "~/.emacs.d/LanguageTool-2.8/languagetool-commandline.jar")
+(use-package langtool
+  :ensure t
+  :config
+  (setq langtool-language-tool-jar "~/.emacs.d/LanguageTool-2.8/languagetool-commandline.jar"))
 
 ;; js2-mode
-(custom-set-variables
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p t)
- )
-
-;; fill-column-indicator
-;; (require 'fill-column-indicator)
-;; (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
-;; (global-fci-mode 1)
+(use-package js2-mode
+  :ensure t
+  :config
+  (setq js2-basic-offset 2
+        js2-bounce-indent-p t))
 
 ;; flycheck
-(require 'flycheck)
-(when (featurep 'flycheck)
-  (global-flycheck-mode))
-(add-to-list 'load-path "~/.emacs.d/flycheck-haskell")
-(load "flycheck-haskell")
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; flycheck-haskell
+(use-package flycheck-haskell
+  :ensure t
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
 
 ;; flycheck-color-mode
-(require 'flycheck-color-mode-line)
-(when (featurep 'flycheck-color-mode-line)
-  (flycheck-color-mode-line-mode))
+(use-package flycheck-color-mode-line
+  :ensure t
+  :init (flycheck-color-mode-line-mode))
 
 ;; guru-mode
-(require 'guru-mode)
-(guru-global-mode t)
-(setq guru-warn-only t)
+(use-package guru-mode
+  :ensure t
+  :init (guru-global-mode)
+  :config (setq guru-warn-only t))
 
 ;; idris-mode
-(add-to-list 'load-path "~/.emacs.d/idris-mode")
-(require 'idris-mode)
+(use-package idris-mode
+  :ensure t)
 
 ;; magit
-(add-hook 'magit-status-mode-hook #'magit-filenotify-mode)
+(use-package magit
+  :ensure t)
 
-;; makefile-mode
-(add-hook 'makefile-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode t)))
+(use-package magit-filenotify
+  :ensure t
+  :config (add-hook 'magit-status-mode-hook #'magit-filenotify-mode))
+
+;; makefile
+(add-hook 'makefile-mode-hook (progn (setq indent-tabs-mode t)))
 
 ;; markdown-mode
-(require 'markdown-mode)
-(when (featurep 'markdown-mode)
+(use-package markdown-mode
+  :ensure t
+  :config
   (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-  )
+  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode)))
 
 ;; multiple-cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; predictive
-(add-to-list 'load-path "~/.emacs.d/predictive")
-(require 'predictive)
-(set-default 'predictive-auto-add-to-dict t)
-(setq predictive-auto-learn t
-      predictive-add-to-dict-ask nil
-      predictive-use-auto-learn-cache nil
-      predictive-which-dict t)
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+	 ("C->" . mc/mark-next-like-this)
+	 ("C-<" . mc/mark-previous-like-this)
+	 ("C-c C-<" . mc/mark-all-like-this)))
 
 ;; pretty-mode
-(require 'pretty-mode)
-(global-pretty-mode t)
+(use-package pretty-mode
+  :ensure t
+  :init (global-pretty-mode))
 
 ;; projectile
-(require 'projectile)
-(projectile-global-mode)
-(setq projectile-enable-caching t)
+(use-package projectile
+  :ensure t
+  :init (projectile-global-mode)
+  :config (setq projectile-enable-caching t))
+
+;; quack
+(use-package quack :ensure t)
 
 ;; rainbow-delimiters
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(use-package rainbow-delimiters
+  :ensure t
+  :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 ;; saveplace
-(require 'saveplace)
-(when (featurep 'saveplace)
+(use-package saveplace
+  :ensure t
+  :config
   (setq save-place-file "~/.emacs.cache/saveplace")
   (setq-default save-place t))
 
 ;; scss-mode
-(setq scss-sass-options '("--style" "compressed" "--cache-location" "'/tmp/.sass-cache'"))
+(use-package scss-mode
+  :ensure t
+  :config
+  (setq scss-sass-options
+	'("--style" "compressed" "--cache-location" "'/tmp/.sass-cache'")))
+
 
 ;; undo-tree
-(require 'undo-tree)
-(global-undo-tree-mode t)
-(setq undo-tree-visualizer-relative-timestamps t)
-(setq undo-tree-visualizer-timestamps t)
-(setq undo-tree-auto-save-history t)
-(setq undo-tree-history-directory-alist '((".*" . "~/.emacs.cache/undo/")))
+(use-package undo-tree
+  :ensure t
+  :init (global-undo-tree-mode t)
+  :config
+  (setq undo-tree-visualizer-relative-timestamps t)
+  (setq undo-tree-visualizer-timestamps t)
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist '((".*" . "~/.emacs.cache/undo/"))))
 
 ;; whitespace
-(require 'whitespace)
-(setq whitespace-line-column 80)
-(setq whitespace-style '(face tabs empty trailing lines-tail space-before-tab))
-(when (featurep 'whitespace)
-  (global-whitespace-mode +1))
-(add-hook 'before-save-hook 'whitespace-cleanup)
+(use-package whitespace
+  :ensure t
+  :init (global-whitespace-mode t)
+  :config
+  (setq whitespace-line-column 80)
+  (setq whitespace-style '(face tabs empty trailing lines-tail space-before-tab))
+  (add-hook 'before-save-hook 'whitespace-cleanup))
 
 ;; scala
-(require 'scala-mode2)
-;; (require 'sbt-mode)
+(use-package scala-mode2 :ensure t)
+(use-package sbt-mode :ensure t)
+
 (defun scalariver ()
   "Send current buffer to scalariver and replace with response."
   (require 'request)
@@ -330,6 +307,7 @@
     (goto-char (point-min))
     (forward-line (1- line))
     (goto-char (+ column (point)))))
+
 ;; (add-hook 'scala-mode-hook
 ;;           '(lambda ()
 ;;              (local-set-key (kbd "M-.") 'sbt-find-definitions)
@@ -343,74 +321,67 @@
              (local-set-key (kbd "M-RET") 'comint-accumulate)
              ))
 
-;; goodbye, memory hungry monster
-(add-to-list 'load-path "~/.emacs.d/ensime/")
-;; (setq ensime-completion-style 'auto-complete)
-(setq ensime-sbt-perform-on-save "compile")
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-;; (add-hook 'ensime-source-buffer-saved-hook 'ensime-format-source)
-
-;; make ensime work remotely
-;; (defun ensime-stackoverflow-connect (host port)
-;;   (interactive (list
-;;                 (read-from-minibuffer "Host: " ensime-default-server-host)
-;;                 (read-from-minibuffer "Port: " (format "%d" ensime-default-port)
-;;                                       nil t)))
-;;   (let ((c (ensime-connect host port))
-;;         (config (ensime-config-load "~/devel/gmantra/ixchel/.ensime")))
-;;     (ensime-set-config c config)
-;;     (setq ensime-buffer-connection c))
-;;   )
+(use-package ensime
+  :ensure t
+  :config
+  (setq ensime-sbt-perform-on-save "compile")
+  (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+  (add-hook 'ensime-source-buffer-saved-hook 'ensime-format-source))
 
 ;; slime
-(setq inferior-lisp-program (executable-find "sbcl"))
-(setq slime-contribs '(slime-fancy))
+(use-package slime
+  :ensure t
+  :config
+  (setq inferior-lisp-program (executable-find "sbcl"))
+  (setq slime-contribs '(slime-fancy)))
 
 ;; show-paren-mode
-(show-paren-mode 1)
-(setq show-paren-style 'expression)
+(use-package paren
+  :init (show-paren-mode t)
+  :config (setq show-paren-style 'expression))
 
 ;; smart-mode-line
-(require 'smart-mode-line)
-(setq sml/no-confirm-load-theme t)
-(sml/setup)
-(sml/apply-theme 'respectful)
+(use-package smart-mode-line
+  :ensure t
+  :init (progn
+	  (setq sml/no-confirm-load-theme t)
+	  (sml/setup)
+	  (sml/apply-theme 'respectful)))
 
 ;; smartparens
 (load "sp-init")
 
 ;; smex
-(require 'smex)
-(smex-initialize)
-(setq smex-save-file "~/.emacs.cache/smex-items")
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :config (setq smex-save-file "~/.emacs.cache/smex-items")
+  :bind (("M-x" . smex)
+	 ("M-X" . smex-major-mode-commands)
+	 ("C-c C-c M-x" . execute-extended-command)))
 
 ;; enh-ruby-mode
-(add-to-list 'load-path "~/.emacs.d/enhanced-ruby-mode")
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
-(when (featurep 'enh-ruby-mode)
-  (setq enh-ruby-deep-indent-paren nil))
+(use-package enh-ruby-mode
+  :ensure t
+  :mode "\\.rb$"
+  :interpreter "ruby"
+  :config (setq enh-ruby-deep-indent-param nil))
 
 ;; web-mode
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq web-mode-markup-indent-offset 2)
-            (setq web-mode-css-indent-offset 2)
-            (setq web-mode-code-indent-offset 2)
-            ))
+(use-package web-mode
+  :ensure t
+  :mode "\\.html?\\'"
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
 
 ;; yasnippet
-(require 'yasnippet)
-(setq yas-snippet-dirs "~/.emacs.d/yasnippet-snippets")
-(yas-global-mode 1)
+(use-package yasnippet
+  :ensure t
+  :init (progn
+	  (setq yas-snippet-dirs "~/.emacs.d/yasnippet-snippets")
+	  (yas-global-mode t)))
 
 (provide 'utils)
 ;;; utils.el ends here
