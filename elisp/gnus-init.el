fdaad818894546bcb557e31df83a7bb06b597a2a
Vikraman Choudhury <git@vikraman.org>
2015-02-18 03:46:00 +0530

    Port gnus-init to `use-package'
    Untested since I don't use it anymore.

--- elisp/gnus-init.el
+++ elisp/gnus-init.el
@@ -2,10 +2,15 @@
 ;;; Commentary:
 
 ;;; Code:
-;; Setup email / news
-;; (setq imap-shell-program "/usr/libexec/dovecot/imap")
-;; (setq gnus-select-method '(nnimap "Mail" (nnimap-stream shell)))
-(setq-default gnus-select-method '(nntp "news.gmane.org"))
+
+(require use-package)
+
+(use-package gnus
+  :config
+  ;; Setup email / news
+  ;; (setq imap-shell-program "/usr/libexec/dovecot/imap")
+  ;; (setq gnus-select-method '(nnimap "Mail" (nnimap-stream shell)))
+  (setq-default gnus-select-method '(nntp "news.gmane.org")))
 
 (provide 'gnus-init)
 ;;; gnus-init.el ends here
