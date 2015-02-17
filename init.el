4a9dac202f1e14931da1f56f4d1c5bbe2676975d
Vikraman Choudhury <git@vikraman.org>
2015-02-17 20:39:23 +0530

    Switch to `use-package` and disable default init

--- init.el
+++ init.el
@@ -15,64 +15,8 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.cache/autosaves/\\1" t))))
- '(backup-directory-alist (quote ((".*" . "~/.emacs.cache/backups/"))))
- '(custom-safe-themes
-   (quote
-    ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
- '(fringe-mode (quote (nil . 0)) nil (fringe))
- '(haskell-complete-module-preferred
-   (quote
-    ("Data.ByteString" "Data.ByteString.Lazy" "Data.Conduit" "Data.Function" "Data.List" "Data.Map" "Data.Maybe" "Data.Monoid" "Data.Ord")))
- '(haskell-interactive-mode-eval-mode (quote haskell-mode))
- '(haskell-interactive-mode-eval-pretty nil)
- '(haskell-interactive-mode-include-file-name nil)
- '(haskell-notify-p t)
- '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng")))
- '(haskell-process-args-ghci (quote ("-ferror-spans")))
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-generate-tags nil)
- '(haskell-process-log t)
- '(haskell-process-path-ghci "ghci-ng")
- '(haskell-process-reload-with-fbytecode nil)
- '(haskell-process-suggest-haskell-docs-imports t)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-type (quote cabal-repl))
- '(haskell-process-use-presentation-mode t)
- '(haskell-stylish-on-save t)
- '(haskell-tags-on-save t)
- '(hindent-style "chris-done")
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p t)
- '(send-mail-function (quote sendmail-send-it))
- '(shm-auto-insert-bangs t)
- '(shm-auto-insert-skeletons t)
- '(shm-use-presentation-mode t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; Load my config
-(setq load-path (cons "~/.emacs.d/elisp" load-path))
-
-(load "emacs")
-(load "theme")
-(load "utils")
-(load "erc-init")
-(load "mu4e-init")
-
-(add-hook 'kill-emacs-hook
-          '(lambda ()
-             (byte-recompile-directory (expand-file-name "~/.emacs.d/") 0)))
+(require 'use-package)
+(setq inhibit-default-init t)
 
 (provide 'init)
 ;;; init.el ends here
