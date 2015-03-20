e66925096e842769c5ea5259981401fa9c11fdd3
Vikraman Choudhury <git@vikraman.org>
2015-03-21 02:51:50 +0530

    sp-init should not be deferred

--- init.el
+++ init.el
@@ -173,7 +173,7 @@
 (use-package dart-init :defer t :load-path "elisp/")
 (use-package haskell-init :defer t :load-path "elisp/")
 (use-package mu4e-init :load-path "elisp/")
-(use-package sp-init :defer t :load-path "elisp/")
+(use-package sp-init :load-path "elisp/")
 (use-package tex-init :defer t :load-path "elisp/")
 (use-package utils :load-path "elisp/")
 
