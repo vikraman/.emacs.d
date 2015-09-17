962c528f291c92e679c3820ca915d69e1dcf99d1
Vikraman Choudhury <git@vikraman.org>
2015-09-17 00:52:42 -0400

    Fix broken matching parens with smartparens

--- elisp/scheme-init.el
+++ elisp/scheme-init.el
@@ -12,7 +12,13 @@
   (use-package quack
     :ensure t
     :config
-    (setq quack-smart-open-paren-p t))
+    (use-package sp-init
+      :config
+      (smartparens-strict-mode +1)
+      (add-to-list 'sp--special-self-insert-commands 'quack-insert-opening-paren)
+      (add-to-list 'sp--special-self-insert-commands 'quack-insert-closing-paren)
+      (add-to-list 'sp--special-self-insert-commands 'quack-insert-opening-bracket)
+      (add-to-list 'sp--special-self-insert-commands 'quack-insert-closing-bracket)))
   (setq geiser-mode-start-repl-p t
         geiser-repl-history-filename "~/.emacs.cache/geiser-history"))
 
