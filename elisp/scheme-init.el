a962a393edc7aed50cb5e7a2ff33885767149d8e
Vikraman Choudhury <git@vikraman.org>
2015-10-25 20:06:17 -0400

    Use racket-mode instead of geiser for racket

--- elisp/scheme-init.el
+++ elisp/scheme-init.el
@@ -17,5 +17,17 @@
   (setq geiser-mode-start-repl-p t
         geiser-repl-history-filename "~/.emacs.cache/geiser-history"))
 
+;; racket-mode
+(use-package racket-mode
+  :ensure t
+  :init
+  (progn
+    (delete '("\\.rkt\\'" . scheme-mode) auto-mode-alist)
+    (delete '("\\.rktd\\'" . scheme-mode) auto-mode-alist)
+    (add-to-list 'auto-mode-alist '("\\.rkt[dl]?\\'" . racket-mode)))
+  :config
+  (add-hook 'racket-mode-hook #'racket-unicode-input-method-enable)
+  (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable))
+
 (provide 'scheme-init)
 ;;; scheme-init.el ends here
