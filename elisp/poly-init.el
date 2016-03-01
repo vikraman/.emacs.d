90c8bf05c537f3f22127007bed4458930b5143fd
Vikraman Choudhury <git@vikraman.org>
2016-03-01 18:31:28 -0500

    TeX and haskell poly mode

--- elisp/poly-init.el
+++ elisp/poly-init.el
@@ -32,7 +32,26 @@
     :group 'polymodes
     :type 'object)
   (define-polymode poly-TeX+agda2-mode pm-poly/TeX+agda2)
-  (add-to-list 'auto-mode-alist '("\\.lagda\\'" . poly-TeX+agda2-mode)))
+  (add-to-list 'auto-mode-alist '("\\.lagda\\'" . poly-TeX+agda2-mode))
+  (defcustom pm-inner/TeX+haskell
+    (pm-hbtchunkmode "TeX haskell"
+                     :mode 'haskell-mode
+                     :head-reg "^[ \t]*\\\\begin{lstlisting}[ \t]*$"
+                     :tail-reg "^[ \t]*\\\\end{lstlisting}[ \t]*$"
+                     :head-mode 'host
+                     :tail-mode 'host
+                     :adjust-face nil)
+    "TeX with haskell chunk"
+    :group 'innermodes
+    :type 'object)
+  (defcustom pm-poly/TeX+haskell
+    (pm-polymode-one "TeX"
+                     :hostmode 'pm-host/TeX
+                     :innermode 'pm-inner/TeX+haskell)
+    "TeX with agda mode"
+    :group 'polymodes
+    :type 'object)
+  (define-polymode poly-TeX+haskell-mode pm-poly/TeX+haskell))
 
 (provide 'poly-init)
 ;;; poly-init.el ends here
