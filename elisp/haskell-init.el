c8d0aadfde1bbd158765c9c736ae6736249cbe5e
Vikraman Choudhury <git@vikraman.org>
2016-07-29 22:57:54 -0400

    Setup liquid haskell

--- elisp/haskell-init.el
+++ elisp/haskell-init.el
@@ -8,8 +8,19 @@
 (use-package intero
   :ensure t
   :config
+  (use-package button-lock
+    :ensure t
+    :config
+    (use-package liquid-types
+      :load-path "liquid-tip.el/"))
+  (use-package flycheck-liquidhs
+    :load-path "flycheck-liquidhs.el/")
   (setq intero-package-version "0.1.16")
-  (add-hook 'haskell-mode-hook 'intero-mode))
+  (add-hook 'haskell-mode-hook
+            '(lambda ()
+               (intero-mode)
+               (flycheck-select-checker 'haskell-liquid)
+               (liquid-types-mode))))
 
 (provide 'haskell-init)
 ;;; haskell-init.el ends here
