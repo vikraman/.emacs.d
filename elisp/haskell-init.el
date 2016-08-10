5fd74f571b3fa36c45d80ce6e1265f2cfd9a3fbb
Vikraman Choudhury <git@vikraman.org>
2016-08-10 03:57:39 -0400

    Enable ghc-mod again

--- elisp/haskell-init.el
+++ elisp/haskell-init.el
@@ -15,10 +15,13 @@
       :load-path "liquid-tip.el/"))
   (use-package flycheck-liquidhs
     :load-path "flycheck-liquidhs.el/")
+  (use-package ghc
+    :ensure t)
   (setq intero-package-version "0.1.16")
   (add-hook 'haskell-mode-hook
             '(lambda ()
                (intero-mode)
+               (ghc-init)
                (flycheck-select-checker 'haskell-liquid)
                (liquid-types-mode))))
 
